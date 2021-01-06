db0b8d9715404f69173631a33951e4fa07774f3c
Guilherme-Bodart <guilhermebodart73@gmail.com>
2021-01-06 17:25:22 -0300

    setState Pages

--- gestao-competencias-front/src/pages/CriarConta/CriarConta.js
+++ gestao-competencias-front/src/pages/CriarConta/CriarConta.js
@@ -6,11 +6,46 @@ import Image from 'react-bootstrap/Image'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-import logo from "../../assets/leds-icon.png";
+import logo from "../../assets/leds-logo.svg";
 import '../../styles/login.css';
 
-
+const initialState = {
+  email: '',
+  senha: '',
+  senhaConfirmada: '',
+  nome: '',
+}
 class CriarConta extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = initialState
+  }
+
+  onChangeNome = (event) => {
+    this.setState({
+      nome: event.target.value
+    })
+  }
+
+  onChangeEmail = (event) => {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  onChangeSenha = (event) => {
+    this.setState({
+      senha: event.target.value
+   })
+  }
+
+  onChangeSenhaConfirmada = (event) => {
+    this.setState({
+      senhaConfirmada: event.target.value
+   })
+  }
+
   render(props) {
   return (
     <div className="App">
@@ -20,27 +55,43 @@ class CriarConta extends Component {
         <Form className="App-form">
 
             <Form.Group controlId="formBasicEmail"className="App-form-groupC">
-                <Form.Control type="text" placeholder="Nome Completo" className="App-form-control"/>
+                <Form.Control type="text" placeholder="Nome Completo" 
+                className="App-form-control"
+                onChange = {value => this.onChangeNome(value)}/>
                 <Form.Text className="text-muted">
                 </Form.Text>
             </Form.Group>
 
              <Form.Group controlId="formBasicEmail"className="App-form-groupC">
-                <Form.Control type="email" placeholder="Seu e-mail" className="App-form-control"/>
+                <Form.Control type="email" placeholder="Seu e-mail" 
+                className="App-form-control"
+                onChange = {value => this.onChangeEmail(value)}/>
                 <Form.Text className="text-muted">
                 </Form.Text>
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword" className="App-form-groupC">
-                <Form.Control type="password" placeholder="Senha" className="App-form-control"/>
+                <Form.Control type="password" placeholder="Senha" 
+                className="App-form-control"
+                onChange = {value => this.onChangeSenha(value)}/>
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword" className="App-form-groupC">
-                <Form.Control type="password" placeholder="Confirmação de Senha" className="App-form-control"/>
+                <Form.Control type="password" placeholder="Confirmação de Senha"
+                 className="App-form-control"
+                 onChange = {value => this.onChangeSenhaConfirmada(value)}/>
             </Form.Group>
 
-            <Button variant="outline-primary" type="submit" className="App-button-login">
-                <p className="App-text-button">Criar : : Conta</p>
+            <Button variant="outline-primary" type="submit" className="App-button-login" 
+            onClick = { () => {
+              if(this.state.senha === this.state.senhaConfirmada){
+                alert(JSON.stringify(this.state))
+              }else{
+                alert("Senhas não são iguais")
+              }
+              }
+            }>
+                <p className="App-text-button">Criar Conta</p>
             </Button>
         </Form>
       </header>
--- gestao-competencias-front/src/pages/Login/Login.js
+++ gestao-competencias-front/src/pages/Login/Login.js
@@ -6,11 +6,41 @@ import Image from 'react-bootstrap/Image'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-import logo from "../../assets/leds-icon.png";
+import logo from "../../assets/leds-logo.svg";
 import '../../styles/login.css';
 
 
+const initialState = {
+  email: '',
+  senha: '',
+  logado: false,
+}
+
 class Login extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = initialState
+  }
+
+  onChangeLogado = () => {
+    this.setState({ 
+      logado: true
+    })
+  }
+
+  onChangeEmail = (event) => {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  onChangeSenha = (event) => {
+    this.setState({
+      senha: event.target.value
+   })
+  }
+
   render(props) {
   return (
     <div className="App">
@@ -21,20 +51,24 @@ class Login extends Component {
 
           <Form.Group controlId="formBasicEmail" className="App-form-group">
             <Form.Label>E-Mail</Form.Label>
-            <Form.Control type="email" placeholder="Entre com seu e-mail" className="App-form-control"/>
+            <Form.Control type="email" placeholder="Entre com seu e-mail"
+                className="App-form-control"  
+                onChange = {value => this.onChangeEmail(value)}/>
             <Form.Text className="text-muted">
             </Form.Text>
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword" className="App-form-group">
             <Form.Label >Senha</Form.Label>
-            <Form.Control type="password" placeholder="Senha" className="App-form-control"/>
+            <Form.Control type="password" placeholder="Senha" 
+            className="App-form-control" 
+            onChange = {value => this.onChangeSenha(value)}/>
           </Form.Group>
           
-          <Button variant="outline-success" type="submit" className="App-button-login">
+          <Button variant="outline-success" type="submit" className="App-button-login" onClick = { () => alert(JSON.stringify(this.state))}>
             <p className="App-text-button">Entrar</p>
           </Button>
-          <Button variant="outline-primary" type="submit" className="App-button-login">
+          <Button variant="outline-primary" type="submit" className="App-button-login" >
             <p className="App-text-button">Crie sua conta aqui</p>
           </Button>
         </Form>
