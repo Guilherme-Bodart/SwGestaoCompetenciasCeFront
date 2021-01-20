8a709cf7f4a7985f700d75ce42ebc6a19b68a698
Guilherme-Bodart <guilhermebodart73@gmail.com>
2021-01-20 15:00:01 -0300

    + Campos em FormCriacao

--- gestao-competencias-front/src/pages/CriarConta/CriarConta.js
+++ gestao-competencias-front/src/pages/CriarConta/CriarConta.js
@@ -14,6 +14,10 @@ const initialState = {
   senha: '',
   senhaConfirmada: '',
   nome: '',
+  dataNascimento: '',
+  telefone: '',
+  cpf: '',
+  estudante: 1,
 }
 class CriarConta extends Component {
 
@@ -46,41 +50,114 @@ class CriarConta extends Component {
    })
   }
 
+  onChangeTelefone = (event) => {
+    this.setState({
+      telefone: event.target.value
+   })
+  }
+
+  onChangeDataNascimento = (event) => {
+    this.setState({
+      dataNascimento: event.target.value
+   })
+  }
+
+  onChangeCPF = (event) => {
+    this.setState({
+      cpf: event.target.value
+   })
+  }
+
+  onChangeEstudante = (event) => {
+    this.setState({
+      estudante: event.target.value
+   })
+  }
+
   render(props) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="App-text-logo">LEDS SKILLS</p>
-        <Form className="App-form">
+        <Form className="App-form">          
+          <Col >
 
-            <Form.Group controlId="formBasicEmail"className="App-form-groupC">
-                <Form.Control type="text" placeholder="Nome Completo" 
-                className="App-form-control"
-                onChange = {value => this.onChangeNome(value)}/>
-                <Form.Text className="text-muted">
-                </Form.Text>
-            </Form.Group>
+            <Row >
+              <Col md={{ span: 5 }}>
+              <Form.Group controlId="formBasicEmail"className="App-form-groupC">
+                  <Form.Control type="text" placeholder="Nome Completo" 
+                  className="App-form-control"
+                  onChange = {value => this.onChangeNome(value)}/>
+                  <Form.Text className="text-muted">
+                  </Form.Text>
+              </Form.Group>
+              </Col>
+              <Col md={{ span: 5, offset: 1 }}>
+              <Form.Group controlId="formBasicEmail"className="App-form-groupC">
+                  <Form.Control type="date"  
+                  className="App-form-control"
+                  onChange = {value => this.onChangeDataNascimento(value)}/>
+                  <Form.Text className="text-muted">
+                  </Form.Text>
+              </Form.Group>
+              </Col>
+            </Row>
 
-             <Form.Group controlId="formBasicEmail"className="App-form-groupC">
-                <Form.Control type="email" placeholder="Seu e-mail" 
-                className="App-form-control"
-                onChange = {value => this.onChangeEmail(value)}/>
-                <Form.Text className="text-muted">
-                </Form.Text>
-            </Form.Group>
+            <Row >
+              <Col md={{ span: 5 }}>
+              <Form.Group controlId="formBasicEmail"className="App-form-groupC">
+              <Form.Control type="email" placeholder="Seu e-mail" 
+                  className="App-form-control"
+                  onChange = {value => this.onChangeEmail(value)}/>
+                  <Form.Text className="text-muted">
+                  </Form.Text>
+              </Form.Group>
+              </Col>
+              <Col md={{ span: 5, offset: 1 }}>
+              <Form.Group controlId="formBasicEmail"className="App-form-groupC">
+                  <Form.Control type="text" placeholder="Telefone" 
+                  className="App-form-control"
+                  onChange = {value => this.onChangeTelefone(value)}/>
+                  <Form.Text className="text-muted">
+                  </Form.Text>
+              </Form.Group>
+              </Col>
+            </Row>
 
-            <Form.Group controlId="formBasicPassword" className="App-form-groupC">
-                <Form.Control type="password" placeholder="Senha" 
-                className="App-form-control"
-                onChange = {value => this.onChangeSenha(value)}/>
-            </Form.Group>
+            <Row >
+              <Col md={{ span: 5 }}>
+              <Form.Group controlId="formBasicPassword" className="App-form-groupC">
+                  <Form.Control type="password" placeholder="Senha" 
+                  className="App-form-control"
+                  onChange = {value => this.onChangeSenha(value)}/>
+              </Form.Group>
+              </Col>
+              <Col md={{ span: 5, offset: 1 }}>
+              <Form.Group controlId="formBasicPassword" className="App-form-groupC">
+                  <Form.Control type="password" placeholder="Confirmação de Senha"
+                  className="App-form-control"
+                  onChange = {value => this.onChangeSenhaConfirmada(value)}/>
+              </Form.Group>
+              </Col>             
+            </Row>
 
-            <Form.Group controlId="formBasicPassword" className="App-form-groupC">
-                <Form.Control type="password" placeholder="Confirmação de Senha"
-                 className="App-form-control"
-                 onChange = {value => this.onChangeSenhaConfirmada(value)}/>
-            </Form.Group>
+            <Row >              
+              <Col md={{ span: 5 }}>
+              <Form.Group controlId="formBasicEmail" className="App-form-groupC">
+                  <Form.Control type="text" placeholder="CPF"
+                  className="App-form-control"
+                  onChange = {value => this.onChangeCPF(value)}/>
+              </Form.Group>
+              </Col>
+              <Col md={{ span: 5, offset: 1 }}>
+              <Form.Group controlId="formBasicEmail" className="App-form-groupC">
+                  <Form.Control type="text" value="Estudante"
+                  className="App-form-control" readOnly
+                  onChange = {value => this.onChangeEstudante(1)}/>
+              </Form.Group>
+              </Col>
+            </Row>
 
             <Button variant="outline-primary" type="submit" className="App-button-login" 
             onClick = { () => {
@@ -93,7 +170,11 @@ class CriarConta extends Component {
             }>
                 <p className="App-text-button">Criar Conta</p>
             </Button>
+             
+            </Col>
+            
         </Form>
+       
       </header>
     </div>
   )
