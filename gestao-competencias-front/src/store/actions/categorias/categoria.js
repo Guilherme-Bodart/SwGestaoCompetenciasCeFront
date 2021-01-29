381dd3c13e4e39cd02065346e466a19c173692f7
BrennoMilanezi <milanezibrenno@gmail.com>
2021-01-29 15:34:47 -0300

    Editar Subcategoria

--- gestao-competencias-front/src/store/actions/categorias/categoria.js
+++ gestao-competencias-front/src/store/actions/categorias/categoria.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
-import { GET_CATEGORIA, GET_SUBCATEGORIA, LOGOUT_CATEGORIA } from '../actionsTypes'
+import { GET_CATEGORIA, GET_SUBCATEGORIA, LOGOUT_CATEGORIA, GET_DETALHARSUBCATEGORIA } from '../actionsTypes'
 import { alertin } from '../alertas/alerta'
+
+import { pageSubCategoria } from '../adminViews/adminView'
 
 export const logoutCategoria = () => {
     return  {
@@ -121,5 +123,68 @@ export const getSaveSubCategorias = subcategoria => {
     return {
         type: GET_SUBCATEGORIA,
         payload: subcategoria
+    }
+}
+
+export const getSubCategoria = (id_subcategoria) => {
+    return async (dispatch, getState) => {
+
+        const token = 'Bearer ' + getState().usuario.token
+        await axios.get("https://leds-skills.herokuapp.com/subcategory/"+id_subcategoria, { params: { token } })
+            .then(response => {                
+                const subcategoria = response.data
+                dispatch(getSaveSubCategoria(subcategoria))
+            })
+            .catch( error => {
+                if( error.response ){
+                    var erro_msg = error.response.data.error; // => the response payload 
+                    alert(erro_msg)
+                }
+                /*dispatch(alertin({open: true,
+                    alertTitle: 'Erro',
+                    severity: 'error',
+                    texto: 'Falha no envio, '+erro_msg}))*/
+            })
+    }
+}
+
+export const getSaveSubCategoria = subcategoria => {
+    return {
+        type: GET_DETALHARSUBCATEGORIA,
+        payload: subcategoria
+    }
+}
+
+export const editarSubCategoria = (subcategoria) => {
+
+    return async (dispatch, getState ) =>  {
+
+        alert(JSON.stringify(subcategoria));
+
+        const token = 'Bearer ' + getState().usuario.token
+        await axios.put("https://leds-skills.herokuapp.com/subcategory/"+subcategoria.id, null, 
+                { params: {
+                    token,
+                    nome: subcategoria.nome,
+                    categoria: subcategoria.categoria
+                    },
+                }
+            ).then(response => {
+                
+                dispatch(alertin({open: true,
+                    alertTitle: 'Editado',
+                    severity: 'success',
+                    texto: 'A subcategoria foi editada com sucesso'}));
+                
+                dispatch(pageSubCategoria());
+
+            })
+            .catch( error => {
+                dispatch(alertin({open: true,
+                    alertTitle: 'Erro',
+                    severity: 'error',
+                    texto: 'Erro na edição da subcategoria '}))
+            })
+        
     }
 }
