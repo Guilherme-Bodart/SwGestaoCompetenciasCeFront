9e82c223ddc5891a7bb64b02333e3f1e5ead0512
BrennoMilanezi <milanezibrenno@gmail.com>
2021-03-04 16:24:27 -0300

    Dashboard
    Co-Authored-By: Guilherme-Bodart <28104726+Guilherme-Bodart@users.noreply.github.com>
    Co-Authored-By: gmarchezi <26925551+gmarchezi@users.noreply.github.com>
    Co-Authored-By: pauloricardo50 <26925333+pauloricardo50@users.noreply.github.com>

--- gestao-competencias-front/src/views/Dashboard/Dashboard.js
+++ gestao-competencias-front/src/views/Dashboard/Dashboard.js
@@ -6,6 +6,12 @@ import Container from 'react-bootstrap/Container'
 
 import '../../styles/principal.css'
 
+import { FaMedapps, FaUsers, FaSitemap, FaGripHorizontal } from 'react-icons/fa';
+import { pageSubCategoria, pageProjeto, pageUsuario, pageCategoria } from '../../store/actions/adminViews/adminView'
+import { getSubCategorias, getCategorias } from '../../store/actions/categorias/categoria'
+import { getUsuarios } from '../../store/actions/usuarios/usuario'
+import { getProjetos } from '../../store/actions/projetos/projeto'
+
 const initialState = {
 
 }
@@ -16,28 +22,116 @@ class Dashboard extends Component {
         this.state = initialState
     }
 
+    async componentDidMount(){
+        await this.props.getSubCategorias()
+        await this.props.getCategorias()
+        await this.props.getUsuarios()
+        await this.props.getProjetos()
+    }
+
     render(props){
 
         return(
             
             <Container fluid>
-                <Row>
-                <p className="App-text-logo" style={{marginLeft:"1em", marginTop:"0.5em"}}>Dashboard</p>
+                <Row style={{margin: "1rem"}}>
+                <div class="header-body">
+                    <div class="row">
+                        <div class="col-lg-6 col-xl-3">
+                            <div class="card-stats mb-4 mb-xl-0 card" style={{cursor: "pointer"}} onClick={()=>{this.props.pageUsuario()}}>
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col">
+                                            <h5 class="text-uppercase text-muted mb-0 card-title">Usuários</h5>
+                                            <span class="h2 font-weight-bold mb-0">{this.props.usuario.usuarios.length}</span>
+                                        </div>
+                                        <div class="col-auto col">
+                                            <div style={{fontSize:"1.6em", color:"black"}}>
+                                                <FaUsers/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-xl-3">
+                            <div class="card-stats mb-4 mb-xl-0 card" style={{cursor: "pointer"}} onClick={()=>{this.props.pageProjeto()}}>
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col">
+                                            <h5 class="text-uppercase text-muted mb-0 card-title">Projetos</h5>
+                                            <span class="h2 font-weight-bold mb-0">{this.props.projeto.projetos.length}</span>
+                                        </div>
+                                        <div class="col-auto col">
+                                            <div style={{fontSize:"1.6em", color:"black"}}>
+                                                <FaMedapps/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-xl-3">
+                            <div class="card-stats mb-4 mb-xl-0 card" style={{cursor: "pointer"}} onClick={()=>{this.props.pageCategoria()}}>
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col">
+                                            <h5 class="text-uppercase text-muted mb-0 card-title">Categorias</h5>
+                                            <span class="h2 font-weight-bold mb-0">{this.props.categoria.categorias.length}</span>
+                                        </div>
+                                        <div class="col-auto col">
+                                            <div style={{fontSize:"1.4em", color:"black"}}>
+                                                <FaSitemap/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-xl-3">
+                            <div class="card-stats mb-4 mb-xl-0 card" style={{cursor: "pointer"}} onClick={()=>{this.props.pageSubCategoria()}}>
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col">
+                                            <h5 class="text-uppercase text-muted mb-0 card-title">Subcategorias</h5>
+                                            <span class="h2 font-weight-bold mb-0">{this.props.categoria.subcategorias.length}</span>
+                                        </div>
+                                        <div class="col-auto col">
+                                            <div style={{fontSize:"1.6em", color:"black"}}>
+                                                <FaGripHorizontal/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 </Row>
-                Em construção
             </Container>
           
         )
     }
 }
 
-const mapStateToProps = ({  }) => {
+const mapStateToProps = ({ categoria, projeto, usuario }) => {
     return {
+        categoria, 
+        projeto,
+        usuario
     }
   }
   
   const mapDispatchToProps = dispatch => {
     return {      
+        getUsuarios: () => dispatch(getUsuarios()),       
+        getProjetos: () => dispatch(getProjetos()),
+        getCategorias: () => dispatch(getCategorias()),       
+        getSubCategorias: () => dispatch(getSubCategorias()),       
+        pageSubCategoria: () => dispatch(pageSubCategoria()),
+        pageCategoria: () => dispatch(pageCategoria()),
+        pageProjeto: () => dispatch(pageProjeto()),
+        pageUsuario: () => dispatch(pageUsuario())
     }
   }
   export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
--- gestao-competencias-front/src/viewsUser/Dashboard/Dashboard.js
+++ gestao-competencias-front/src/viewsUser/Dashboard/Dashboard.js
@@ -6,6 +6,11 @@ import Container from 'react-bootstrap/Container'
 
 import '../../styles/principal.css'
 
+import { FaMedapps, FaUsers, FaSitemap, FaGripHorizontal } from 'react-icons/fa';
+import { getAlunoAtividades } from '../../store/actions/atividades/atividade'
+import { pageAtividade, pageProjeto } from '../../store/actions/userViews/userView'
+import { getAlunoProjetos } from '../../store/actions/projetos/projeto'
+
 const initialState = {
 
 }
@@ -16,28 +21,75 @@ class Dashboard extends Component {
         this.state = initialState
     }
 
+    async componentDidMount(){
+        await this.props.getAlunoAtividades()
+        await this.props.getAlunoProjetos()
+    }
+
     render(props){
 
         return(
             
             <Container fluid>
-                <Row>
-                <p className="App-text-logo" style={{marginLeft:"1em", marginTop:"0.5em"}}>Dashboard</p>
+                <Row style={{margin: "1rem"}}>
+                <div class="header-body">
+                    <div class="row">
+                        <div style={{marginLeft: "1rem", marginRight: "2rem"}}>
+                            <div class="card-stats mb-xl-0 card" style={{cursor: "pointer"}} onClick={()=>{this.props.pageProjeto()}}>
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col">
+                                            <h5 class="text-uppercase text-muted mb-0 card-title">Projetos</h5>
+                                            <span class="h2 font-weight-bold mb-0">{this.props.projeto.projetos.length}</span>
+                                        </div>
+                                        <div class="col-auto col">
+                                            <div style={{fontSize:"1.6em", color:"black"}}>
+                                                <FaMedapps/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="card-stats mb-xl-0 card" style={{cursor: "pointer"}} onClick={()=>{this.props.pageAtividade()}}>
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col">
+                                            <h5 class="text-uppercase text-muted mb-0 card-title">Atividades</h5>
+                                            <span class="h2 font-weight-bold mb-0">{this.props.atividade.atividades.length}</span>
+                                        </div>
+                                        <div class="col-auto col">
+                                            <div style={{fontSize:"1.4em", color:"black"}}>
+                                                <FaSitemap/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 </Row>
-                Em construção
             </Container>
           
         )
     }
 }
 
-const mapStateToProps = ({  }) => {
+const mapStateToProps = ({ atividade, projeto }) => {
     return {
+        atividade, 
+        projeto
     }
   }
   
   const mapDispatchToProps = dispatch => {
-    return {      
+    return {          
+        getAlunoProjetos: () => dispatch(getAlunoProjetos()),
+        getAlunoAtividades: () => dispatch(getAlunoAtividades()),       
+        pageProjeto: () => dispatch(pageProjeto()),
+        pageAtividade: () => dispatch(pageAtividade()),
     }
   }
   export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
